813cd9b306f83d9adfd134e93d7b1493da64c78d
marcin102 <marcin@godtsagt.no>
2021-10-29 17:42:36 +0200

    added compiled styles after conflicts

--- app/public/wp-content/themes/JointsWP/parts/nav-offcanvas-topbar.php
+++ app/public/wp-content/themes/JointsWP/parts/nav-offcanvas-topbar.php
@@ -9,32 +9,19 @@
 	<div class="container" id="top-bar-menu">
 		<div class="site-header__body">
 
-			<div class="title-bar" data-responsive-toggle="main-nav" data-hide-for="large">
+		<div class="title-bar" data-responsive-toggle="main-nav" data-hide-for="large">
 				<button class="menu-icon" type="button" data-toggle="main-nav"></button>
 				<div class="title-bar-title">Menu</div>
 			</div>
 
 			<div class="top-bar" id="main-nav">
 				<div class="top-bar-left">
-					<ul class="dropdown menu" data-dropdown-menu>
-					<li class="menu-text">Site Title</li>						
-					<li>
-						<a href="#">One</a>
-						<ul class="menu vertical">
-						<li><a href="#">One</a></li>
-						<li><a href="#">Two</a></li>
-						<li><a href="#">Three</a></li>
-						</ul>
-					</li>						
-					<li><a href="#">Two</a></li>
-					<li><a href="#">Three</a></li>
-					</ul>
-				</div>
-				<div class="top-bar-right">
-					<ul class="menu">
-					<li><input type="search" placeholder="Search"></li>
-					<li><button type="button" class="button">Search</button></li>
-					</ul>
+					<nav class="main-navigation vertical menu nav  dropdown-pane" id="mobile-dropdown" data-dropdown">
+						<?php joints_top_nav(); ?>
+					</nav>
+					<nav class="main-navigation nav desktop-dropdown medium-centered show-for-large">
+                    	<?php joints_top_nav(); ?>  
+					</nav>
 				</div>
 			</div>
 
@@ -50,13 +37,13 @@
 				<!-- mobile menu -->
 				
 
-				<!-- menu desktop 
+				<!--
 				<nav class="main-navigation vertical menu nav  dropdown-pane" id="mobile-dropdown" data-dropdown">
 					<?php joints_top_nav(); ?>
 				</nav>
                 <nav class="main-navigation nav desktop-dropdown medium-centered show-for-large">
                     <?php joints_top_nav(); ?>  
-				</nav>--> 
+				</nav> -->
 
 			</div>
 		</div>
