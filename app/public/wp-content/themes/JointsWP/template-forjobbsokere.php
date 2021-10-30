ef2813135f323f327f3deb3da464348d60bfd090
marcin102 <marcin@godtsagt.no>
2021-10-30 22:24:38 +0200

    added block Steps

--- app/public/wp-content/themes/JointsWP/template-forjobbsokere.php
+++ app/public/wp-content/themes/JointsWP/template-forjobbsokere.php
@@ -17,28 +17,29 @@ get_header(); ?>
         <section class="gradient">
             <!-- Block Hva tilbyr Promo -->
 
-                <?php get_template_part( 'parts/textblock' ); ?>
+            <?php get_template_part( 'parts/textblock' ); ?>
 
             <!-- End Block Hva tilbyr Promo -->
     
             <!-- Block Form Send CV -->
 
-                <?php get_template_part( 'parts/form' ); ?>
+            <?php get_template_part( 'parts/form' ); ?>
 
             <!-- End Form Send CV  -->
             
+            <!-- Block STEPS -->
 
+            <?php get_template_part( 'parts/steps' ); ?>
+
+            <!-- END Block STEPS -->
 
             <!-- Block VÅRE FAGOMRÅDER -->
 
-            <?php get_template_part( 'parts/cta' ); ?>
+            <?php get_template_part( 'parts/fagomraader' ); ?>
 
             <!-- END Block VÅRE FAGOMRÅDER -->
 
         </section> <!-- END Section Gradient -->
-
-
-
 	
     </main> <!-- end Main -->
 
